70abe5067ef95fe678ade6fd9eea7e0ef37170ff
Peter Eriksson <that.peter@gmail.com>
2024-02-20 15:00:59 +0100

    Add layerId to take params

--- lib/api/mixer-controller-api.ts
+++ lib/api/mixer-controller-api.ts
@@ -5,26 +5,31 @@
 */
 
 export interface FileNameParam {
+  layerId: string;
   key: 'filename';
   value: string;
 }
 
 export interface LoopParam {
+  layerId: string;
   key: 'loop';
   value: boolean;
 }
 
 export interface AudioRoutingParam {
+  layerId: string;
   key: 'audio-routing';
   value: number[];
 }
 
 export interface AudioLevelParam {
+  layerId: string;
   key: 'audio-level';
   value: number;
 }
 
 export interface InterlacingParam {
+  layerId: string;
   key: 'interlacing';
   value: 'progressive' | 'interlaced';
 }
