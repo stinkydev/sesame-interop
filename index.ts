a64b638560910dd2b28eb84990fb9b265b548762
Peter Eriksson <that.peter@gmail.com>
2026-02-25 19:45:08 +0100

    Refactor exports in index.ts to use namespace syntax for TypeScript interfaces

--- index.ts
+++ index.ts
@@ -2,13 +2,11 @@
 // Re-exports all TypeScript interfaces and generated Zod schemas
 
 // Export TypeScript interfaces with namespaces to avoid conflicts
-import * as ElmoChannelData from './lib/api/elmo-channel-data';
-import * as MixerControllerApi from './lib/api/mixer-controller-api';
-import * as MixerConfig from './lib/config/mixer-config';
-import * as SesameConfig from './lib/config/sesame-config';
-import * as SesameStreams from './lib/status/sesame-streams';
-
-export { ElmoChannelData, MixerControllerApi, MixerConfig, SesameConfig, SesameStreams };
+export * as ElmoChannelData from './lib/api/elmo-channel-data';
+export * as MixerControllerApi from './lib/api/mixer-controller-api';
+export * as MixerConfig from './lib/config/mixer-config';
+export * as SesameConfig from './lib/config/sesame-config';
+export * as SesameStreams from './lib/status/sesame-streams';
 
 // Export namespaced Zod schemas
 export * from './lib/zod-schemas';
